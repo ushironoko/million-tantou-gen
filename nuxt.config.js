1da0584e316e6aea42a95a2bc47e3f012089d3f4
ushironoko <apple19940820@yahoo.co.jp>
2019-02-25 00:29:46 +0900

    iconかえた

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: '担当アイドルシートをウェブで作れるツール。' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+      { rel: 'icon', type: 'image/x-icon', href: '/icon.png' }
     ]
   },
 
